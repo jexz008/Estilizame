bd0eeba31552bb260c0b5543618681d28580d732
Joel.Corona <Joel.Corona@PC-0294>
2016-05-10 12:05:34 -0500

    Avances Clase Empresa

--- estilizame_sitio2/class/empresa.class.php
+++ estilizame_sitio2/class/empresa.class.php
@@ -18,68 +18,55 @@ INSERT INTO `usuario` (`email`, `contrasena`, `empresa_id_fk`, `nombre`) VALUES
 ('{$email}', SHA1({$password}), NULL, '{$nombre}')
 SQL;
                 $this->db->execute_sql($sql);
-/*              if(!$this->db->execute_sql($sql)){
-                    throw new Exception("Error al insertar Usuario");
-                }*/
-                echo "<br>Usuario: ".$usuarioId = $this->db->last_insert();
+                $usuarioId = $this->db->last_insert();
+                
+		echo "<br>".$sql = <<<SQL
+INSERT INTO `entidad` (`tipo`, `entidad_id_fk`, `estatus`, `fecha_creacion`, `usuario_id_fk`, `usuario_mod_id_fk`) VALUES
+('usuario', {$empresaId}, 1, CURRENT_TIMESTAMP, {$usuarioId}, {$usuarioId})
+SQL;
+                $this->db->execute_sql($sql);
+
+                
+                // Empresa
+		echo "<br>".$sql = <<<SQL
+INSERT INTO empresa 
+(nombre, descripcion, email, telefono, logo, estado, municipio, direccion, foto_perfil, foto_cabecera, video, facebook, twitter, googleplus, instagram, ubicacion_html, categoria_id_fk, jerarquia_id_fk)
+VALUES
+('{$nombre}', '{$descripcion}', '{$email}', '{$telefono}', '{$logo}', '{$estado}', '{$municipio}', '{$direccion}', '{$foto_perfil}', '{$foto_cabecera}', '{$video}', '{$facebook}', '{$twitter}', '{$googleplus}', '{$instagram}', '{$ubicacion_html}', {$categoria_id}, {$jerarquia_id})
+SQL;
+                $this->db->execute_sql($sql);
+                $empresaId = $this->db->last_insert();
                 
 		echo "<br>".$sql = <<<SQL
 INSERT INTO `entidad` (`tipo`, `entidad_id_fk`, `estatus`, `fecha_creacion`, `usuario_id_fk`, `usuario_mod_id_fk`) VALUES
 ('empresa', {$empresaId}, 1, CURRENT_TIMESTAMP, {$usuarioId}, {$usuarioId})
 SQL;
                 $this->db->execute_sql($sql);
-                /*if(!$this->db->execute_sql($sql)){
-                    throw new Exception("Error al insertar Entidad Usuario");
-                }*/
-                /*
-                // Empresa
-		$sql = <<<SQL
-INSERT INTO empresa 
-(nombre, descripcion, email, telefono, logo, estado, municipio, direccion, foto_perfil, foto_cabecera, video, facebook, twitter, googleplus, instagram, ubicacion_html, categoria_id_fk, jerarquia_id_fk)
-VALUES
-({$nombre}, {$descripcion}, {$email}, {$telefono}, {$logo}, {$estado}, {$municipio}, {$direccion}, {$foto_perfil}, {$foto_cabecera}, {$video}, {$facebook}, {$twitter}, {$googleplus}, {$instagram}, {$ubicacion_html}, {$categoria_id}, {$jerarquia_id})
-SQL;
-                if(!$this->db->execute_sql($sql)){
-                    throw new Exception("Error al insertar Empresa");
-                }
-                $empresaId = $this->db->last_insert();
-                
-		$sql = <<<SQL
-INSERT INTO `entidad` (`tipo`, `entidad_id_fk`, `estatus`, `fecha_creacion`, `usuario_id_fk`, `usuario_mod_id_fk`) VALUES
-('empresa', {$empresaId}, 1, CURRENT_TIMESTAMP, {$usuarioId}, {$usuarioId})
-SQL;
-                if(!$this->db->execute_sql($sql)){
-                    throw new Exception("Error al insertar Entidad Empresa");
-                }
                 
                 // Usuario
-		$sql = <<<SQL
+		echo "<br>".$sql = <<<SQL
 UPDATE usuario SET empresa_id_fk = {$empresaId} WHERE id = {$usuarioId}
 SQL;
-                if(!$this->db->execute_sql($sql)){
-                    throw new Exception("Error al insertar Entidad Empresa");
-                }
+                $this->db->execute_sql($sql);
+
                 
                 // Especialidades
                 foreach ($especialidades[$categoria_id] as $especialidad) {
-		$sql = <<<SQL
+		echo "<br>".$sql = <<<SQL
 INSERT INTO empresa_especialidad (empresa_id_fk, especialidad_id_fk) 
 VALUES ({$empresaId}, {$especialidad})
 SQL;
-                    if(!$this->db->execute_sql($sql)){
-                        throw new Exception("Error al insertar Especialidad Empresa");
-                    }
+                    $this->db->execute_sql($sql);
                 }
                 
                 // Banners
-		$sql = <<<SQL
+		echo "<br>".$sql = <<<SQL
 INSERT INTO banner (nombre, imagen_url, celda, fila, empresa_id_fk)
-VALUES (nombre, {$empresaId}, 1, 1, {$empresaId}
+VALUES ('{$nombre}', '{$foto_cabecera}', 1, 1, {$empresaId}
 SQL;
-                if(!$this->db->execute_sql($sql)){
-                    throw new Exception("Error al insertar Entidad Empresa");
-                }                */
-            //$this->db->commit();
+                $this->db->execute_sql($sql);
+
+                $this->db->commit();
                 return $empresaId;
             }catch(Exception $e){
                 $this->db->rollback();
@@ -100,7 +87,7 @@ SQL;
                 
 		$empresa_id = $this->setEmpresa($registro_empresa, $registro_descripcion, $registro_email, $registro_telefono, $logo, $registro_estado, $registro_municipio, $registro_direccion, $registro_foto_perfil, $registro_foto_cabecera, $registro_video, $registro_facebook, $registro_twitter, $registro_google, $registro_instagram, $registro_ubicacion, $registro_categoria, $jerarquia_id, $registro_password, $registro_especialidades, $registro_galeria);
 
-                $handle = new upload($_FILES['registro_foto_cabecera'], 'es_Es');
+                $handle = new upload($files['registro_foto_cabecera'], 'es_Es');
                 if ($handle->uploaded) {
                   $handle->file_new_name_body   = 'image_resized';
                   $handle->image_resize         = true;
